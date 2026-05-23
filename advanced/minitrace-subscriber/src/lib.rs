9da9ecf141a4c7cc4d328e1becfd6b07baeb87cf
Joshua Mo <joshua.mo.876@gmail.com>
2026-05-23 12:38:43 +0100

    feat: human readable time in minitrace-subscriber

--- advanced/minitrace-subscriber/src/lib.rs
+++ advanced/minitrace-subscriber/src/lib.rs
@@ -1,11 +1,17 @@
 use core::{event::Event, subscriber::SubscriberExt};
 
+use jiff::tz::TimeZone;
+
 pub struct StdoutSubscriber;
 
 impl SubscriberExt for StdoutSubscriber {
     fn on_record_event(&self, event: &Event) {
         let message = event.message.as_ref().unwrap();
-        let ts = event.timestamp;
+        let ts = jiff::Timestamp::from_second(event.timestamp as i64)
+            .unwrap()
+            .to_zoned(TimeZone::UTC)
+            .to_string();
+
         println!("{ts} - {message}");
     }
 }
