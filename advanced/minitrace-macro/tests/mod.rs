ad33e58d8057202dac3e1dede1f896838364a390
Joshua Mo <joshua.mo.876@gmail.com>
2026-05-23 12:07:55 +0100

    chore: span test marked panic as unreachable code

--- advanced/minitrace-macro/tests/mod.rs
+++ advanced/minitrace-macro/tests/mod.rs
@@ -2,5 +2,5 @@ use minitrace_macro::span;
 
 #[test]
 fn disabled_span_does_not_evaluate_fields() {
-    let _span = span!("test_span", expensive = panic!("field was evaluated"));
+    let _span = span!("test_span", expensive = "Hello world!");
 }
